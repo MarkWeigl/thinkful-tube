fb05afdbba9f26591f26fdbdc02f7406a528d14f
Mark Weigl <markwco@icloud.com>
2016-11-28 21:55:22 -0700

    added css styling

--- app.js
+++ app.js
@@ -1,35 +1,56 @@
-var YOUTUBE_BASE_URL = 'https://www.googleapis.com/youtube/v3/search';
+$(document).ready(function(){
 
-function getDataFromApi(searchTerm, callback) {
-  var query = {
-    part: 'snippet',
-    key: 'AIzaSyCJKuNBnpbzTKcVaiIV2zYIAxtAm8YWMP0',
-    q:  searchTerm
-  };
-  $.getJSON(YOUTUBE_BASE_URL, query, callback);
-}
+  var YOUTUBE_BASE_URL = 'https://www.googleapis.com/youtube/v3/search';
 
+  function getDataFromApi(searchTerm, callback) {
+    var query = {
+      part: 'snippet',
+      key: 'AIzaSyCJKuNBnpbzTKcVaiIV2zYIAxtAm8YWMP0',
+      q:  searchTerm
+    };
+    $.getJSON(YOUTUBE_BASE_URL, query, callback);
+  }
 
-function displayYouTubeSearchData(data) {
-  var resultElement = '';
-  if (data.items) {
-    data.items.forEach(function(item) {
-     resultElement += '<p><a href="https://youtube.com/embed/' + item.id.videoId +'">'+ item.snippet.title + '</a></p>';
+function getMoreDataFromApi(searchTerm, token, callback) {
+    var query = {
+      part: 'snippet',
+      key: 'AIzaSyCJKuNBnpbzTKcVaiIV2zYIAxtAm8YWMP0',
+      q:  searchTerm,
+      pageToken:  token
+    };
+    console.log(token);
+    $.getJSON(YOUTUBE_BASE_URL, query, callback);
+  }
+
+  function displayYouTubeSearchData(data) {
+    var resultElement = '';
+    if (data.items) {
+      data.items.forEach(function(item) {
+       resultElement += '<p><a href="https://youtube.com/embed/' + item.id.videoId +'"> <img src="' + item.snippet.thumbnails.medium.url +'"> </a></p>';
+      });
+      resultElement += '<button id="more" type="button">More Videos</button>'; 
+    }
+    else {
+      resultElement += '<p>No results</p>';
+    }
+    
+    $('.js-search-results').html(resultElement);
+  }
+
+  $('#more').click(function(e) {
+        getDataFromApi(query, displayYouTubeSearchData, data.nextPageToken);
+      });
+
+  function watchSubmit() {
+    $('.js-search-form').submit(function(e) {
+      e.preventDefault();
+      var query = $(this).find('.js-query').val();
+      getDataFromApi(query, displayYouTubeSearchData);
+      
     });
   }
-  else {
-    resultElement += '<p>No results</p>';
-  }
-  
-  $('.js-search-results').html(resultElement);
-}
 
-function watchSubmit() {
-  $('.js-search-form').submit(function(e) {
-    e.preventDefault();
-    var query = $(this).find('.js-query').val();
-    getDataFromApi(query, displayYouTubeSearchData);
-  });
-}
 
-$(function(){watchSubmit();});
+  $(function(){watchSubmit();});
+
+});
